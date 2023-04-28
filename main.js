3ab75417cac3d5e8ada50eb17b245f202468d0b7
snezk456 <86143353+snezk456@users.noreply.github.com>
2023-04-28 09:40:44 +0530

    Update main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+video = "";
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 380, 380);
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
