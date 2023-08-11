eac2e15a44d433e6e83639b4fafcdfe6d4680e3d
snezk456 <86143353+snezk456@users.noreply.github.com>
2023-08-11 18:17:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 video = "";
-status = "";
+status1 = "";
+object = [];
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
 
 function setup() {
     canvas = createCanvas(480, 380);
@@ -10,12 +25,50 @@ function setup() {
 }
 function draw() {
     image(video, 0, 0, 480, 380);
+    if(status1 != "") {
+        objectDetector.detect(video, gotResult);
+        for(i=0; i < object.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+            fill("#FF0000");
+            percent = floor(object[i].confidence * 100);
+            text (object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+            
+            if(object[i].label == object_name) {
+                video.stop();
+                /*variable_name_holds_webcamLiveView.stop();*/
+                objectDetector.detect(gotResult);
+                document.getElementById("object_status").innerHTML = object_name + " Found";
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(object_name+"Found");
+                synth.speak(utterThis);
+            }
+            else
+            {
+                document.getElementById("object_status").innerHTML = object_name + "Not Found";
+            }
+        }
+
+    }
+
 }
 function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    document.getElementById("object_name").value;
 }
 function modelLoaded() {
     console.log("Model Loaded!");
-    status = true;
+    status1 = true;
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
 }
